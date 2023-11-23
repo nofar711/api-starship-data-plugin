97a7f001b56a95ae275c42429743bf67528936ca
Nofar S <nofars7.11@gmail.com>
2023-11-23 19:35:41 +0200

    get window size for responsive

--- static-component/static-component.js
+++ static-component/static-component.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
+	// badges loop
+	var numberOfImages = 8;
+	for (var i = 0; i < numberOfImages; i++) {
+		$('.badges').append('<img src="badges/box-' + i + '.svg"/>');
+	}
 
-  // badges loop
-  var numberOfImages = 8;
-  for (var i = 0; i < numberOfImages; i++) {
-    $('.badges').append('<img src="badges/box-' + i + '.svg"/>');
-  }
+	var width = $(window).width();
+	var size = getSize(width);
+
+	$(window).resize(function () {
+		width = $(window).width();
+		size = getSize(width);
+    $('.bubbels-left').attr('src', 'bg-images/' +size + '-left.png');
+    $('.bubbels-right').attr('src', 'bg-images/' +size + '-right.png');
+	});
+
+	function getSize(width) {
+		if (width >= 1920) {
+			return 'xl';
+		} else if ((width < 1919) & (width >= 1023)) {
+			return 'lg';
+		} else if ((width < 1024) & (width >= 768)) {
+			return 'md';
+		} else if (width < 767) {
+			return 'sm';
+		}
+	}
 
 });
+
+
